a8e85e56813841d5b089479177a8b5e1c839f184
Monesha <moneshadhali@gmail.com>
2024-03-18 16:06:55 +0000

    extension task: completed button done

--- code/lab_jsevents_start_code/script.js
+++ code/lab_jsevents_start_code/script.js
@@ -2,10 +2,11 @@ const form = document.querySelector("#todo-form");
 const todoList = document.querySelector("#list");
 const date = document.querySelector("#dateDisplay"); //<p>
 const dateBtn = document.querySelector("#date"); //<button>
+const completedList = document.querySelector("#completed-list"); //<ul>
 
+//Add item to list
 form.addEventListener("submit", (evt)=> {
     evt.preventDefault();
-    value = 1;
     // console.log(evt.target["new-todo"].value);
     const newListItem = document.createElement("li");
     newListItem.innerText = evt.target["new-todo"].value;
@@ -15,17 +16,29 @@ form.addEventListener("submit", (evt)=> {
     btnDelete.innerText = "Delete";
     btnDelete.setAttribute("onclick", "removeItemList(this)");
 
+    //create done / complete button
+    const btnDone = document.createElement("button");
+    btnDone.innerText = "Done";
+    btnDone.setAttribute("onclick", "doneItemList(this)");
+
     newListItem.appendChild(btnDelete);
+    newListItem.appendChild(btnDone);
     todoList.appendChild(newListItem);
-    // todoList.appendChild(btnDelete);
 })
 
-//Removing item from list
+//Marking Item in List as Done:
+function doneItemList(evt){
+    const parent = evt.parentElement;
+    evt.remove();
+    completedList.appendChild(parent);
+}
+
+//Removing item from List:
 function removeItemList(evt){
     evt.parentElement.remove();
 }
 
-//Date button function:
+//Display Date:
 dateBtn.addEventListener("click", () => {
     date.innerText = Date();
 });
